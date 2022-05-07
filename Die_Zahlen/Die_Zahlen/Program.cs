8ce873e34ca391eb4202ad8fd089001c44c43fbe
Katherine_20 <mob1911494@gmail.com>
2022-05-07 19:55:25 +0300

    created small calculator for two numbers

---
+++ Die_Zahlen/Die_Zahlen/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Die_Zahlen
+{
+    class Program
+    {
+        static void Main (string [] args)
+        {
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            
+            int result = num1 + num2;
+            Console.WriteLine ("Сумма чисел равна: " + result);
+            result = num1 * num2;
+            Console.WriteLine("Произведение чисел равно: " + result);
+            double average = (double) (num1 + num2) / 2;
+            Console.WriteLine ("Среднее арифметическое чисел равно: " + average);
+
+        }
+    }
+}
